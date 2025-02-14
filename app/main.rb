f391519d5ecd9984c117478af77e2626a47f4dba
Grzegorz Lisowski <lisux1991@tlen.pl>
2025-02-14 21:54:28 +0100

    add type

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,20 @@
+def type(args)
+  if ["exit", "echo", "type"].include?(args[0])
+    "#{args[0]} is a shell builtin"
+  else
+    "#{args[0]}: not found"
+  end
+end
+
 while true do
   $stdout.write("$ ")
 
-  # Wait for user input
   command, *args = gets.chomp.split(" ")
 
   break if command == "exit"
-  if command == "echo"
+  if command == "type"
+    $stdout.write(type(args) + "\n")
+  elsif command == "echo"
     $stdout.write(args.join(" ") + "\n")
   else
     $stdout.write("#{command}: command not found\n")
