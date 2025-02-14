50e11db50777aa9494d8c143c42236e6490d8482
Grzegorz Lisowski <lisux1991@tlen.pl>
2025-02-14 21:45:28 +0100

    handle echo command

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,13 @@
 while true do
-    $stdout.write("$ ")
+  $stdout.write("$ ")
 
-    # Wait for user input
-    command, *args = gets.chomp.split(" ")
+  # Wait for user input
+  command, *args = gets.chomp.split(" ")
 
-    break if command == "exit"
+  break if command == "exit"
+  if command == "echo"
+    $stdout.write(args.join(" ") + "\n")
+  else
     $stdout.write("#{command}: command not found\n")
+  end
 end
